adaaa134c5ccda71537a69a3eca669acd8f7f121
ndts55 <nicolastorres@mailbox.org>
2020-03-26 19:47:30 +0100

    read in lines from stdin or from file and print them

--- src/bin/bptime.rs
+++ src/bin/bptime.rs
@@ -1,24 +1,30 @@
+use std::fs::File;
+use std::io::{self, stdin, Read};
 use std::path::PathBuf;
 use structopt::StructOpt;
-use std::io::{self, stdin, Read};
 
 #[derive(StructOpt, Debug)]
 struct Args {
     /// input file
-    #[structopt(parse(from_os_str), short="f")]
+    #[structopt(parse(from_os_str), short = "f")]
     file: Option<PathBuf>,
-
-    /// input content
-    #[structopt(short="c")]
-    content: Option<String>
 }
 
 fn main() -> io::Result<()> {
     let args = Args::from_args();
-    println!("ARGS: {:?}", args);
+    let mut input = String::new();
+    if let Some(file) = args.file {
+        let mut file_handle = File::open(file)?;
+        file_handle.read_to_string(&mut input)?;
+    } else {
+        stdin().lock().read_to_string(&mut input)?;
+    };
 
-    let mut buffer = String::new();
-    stdin().lock().read_to_string(&mut buffer)?;
-    println!("STDIN: {}", buffer);
+    let lines = input.lines();
+
+    for line in lines {
+        println!("{}", line);
+    }
+
     Ok(())
 }
